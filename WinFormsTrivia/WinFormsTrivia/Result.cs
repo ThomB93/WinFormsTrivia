9ced494fc6b0c50c4cbbba79ad33f148e36b2dc6
ThomasB <ThomasB@DESKTOP-M1KJ94I>
2017-03-04 14:23:45 +0100

    Initial Commit

---
+++ WinFormsTrivia/WinFormsTrivia/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsTrivia
+{
+    public class Result
+    {
+        public string category { get; set; }
+        public string type { get; set; }
+        public string difficulty { get; set; }
+        public string question { get; set; }
+        public string correct_answer { get; set; }
+        public List<string> incorrect_answers { get; set; }
+    }
+}
---
+++ WinFormsTrivia/WinFormsTrivia/RootObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsTrivia
+{
+    public class RootObject
+    {
+        public int response_code { get; set; }
+        public List<Result> results { get; set; }
+    }
+}
