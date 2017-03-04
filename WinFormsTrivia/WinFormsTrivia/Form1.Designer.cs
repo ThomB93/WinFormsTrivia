381dfd6c7705bda9b903f9bba98feb1e30debe7f
ThomasB <ThomasB@DESKTOP-M1KJ94I>
2017-03-04 14:53:29 +0100

    Added comboboxes and listbox to form

--- WinFormsTrivia/WinFormsTrivia/Form1.Designer.cs
+++ WinFormsTrivia/WinFormsTrivia/Form1.Designer.cs
@@ -28,36 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboCategory = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboDifficulty = new System.Windows.Forms.ComboBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // comboCategory
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(36, 50);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
+            this.comboCategory.FormattingEnabled = true;
+            this.comboCategory.Location = new System.Drawing.Point(114, 6);
+            this.comboCategory.Name = "comboCategory";
+            this.comboCategory.Size = new System.Drawing.Size(121, 21);
+            this.comboCategory.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(96, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Choose a category";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Difficulty";
+            // 
+            // comboDifficulty
+            // 
+            this.comboDifficulty.FormattingEnabled = true;
+            this.comboDifficulty.Location = new System.Drawing.Point(114, 37);
+            this.comboDifficulty.Name = "comboDifficulty";
+            this.comboDifficulty.Size = new System.Drawing.Size(121, 21);
+            this.comboDifficulty.TabIndex = 3;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(241, 6);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(74, 52);
+            this.buttonGenerate.TabIndex = 4;
+            this.buttonGenerate.Text = "Generate Questions";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(300, 199);
+            this.listBox1.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 360);
+            this.ClientSize = new System.Drawing.Size(328, 274);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.comboDifficulty);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboCategory);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Trivia";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -65,8 +108,12 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboCategory;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboDifficulty;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- WinFormsTrivia/WinFormsTrivia/Form1.cs
+++ WinFormsTrivia/WinFormsTrivia/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,10 +14,55 @@ namespace WinFormsTrivia
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, int> categoryValue;
         public Form1()
         {
             InitializeComponent();
-            RootObject obj = JsonConvert.DeserializeObject<RootObject>(json);
+            comboCategory.Items.Add("General Knowledge");
+            comboCategory.Items.Add("History");
+            comboCategory.Items.Add("Mythology");
+            comboCategory.Items.Add("Movies");
+            comboCategory.Items.Add("Art");
+            comboCategory.Items.Add("Video Games");
+            comboCategory.Items.Add("Board Games");
+            comboCategory.Items.Add("Animals");
+
+            comboDifficulty.Items.Add("Easy");
+            comboDifficulty.Items.Add("Medium");
+            comboDifficulty.Items.Add("Hard");
+
+            comboCategory.SelectedIndex = 0; //set default value
+            comboDifficulty.SelectedIndex = 0; //set default value
+            
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            categoryValue = new Dictionary<string, int> //match json URL value
+            {
+                {"General Knowledge", 9},
+                {"History", 23},
+                {"Mythology", 20},
+                {"Movies", 11},
+                {"Art", 25},
+                {"Video Games", 15},
+                {"Board Games", 16},
+                {"Animals", 27}
+            };
+
+            var category = comboCategory.SelectedItem.ToString();
+            var chosenCategory = categoryValue[category];
+
+            var difficulty = comboDifficulty.SelectedItem.ToString();
+            difficulty = difficulty.ToLower();
+
+            var json = new WebClient().DownloadString("https://www.opentdb.com/api.php?amount=10&category=" + chosenCategory + "&difficulty=" + difficulty + "&type=multiple");
+            RootObject questionsRootObject = JsonConvert.DeserializeObject<RootObject>(json);
+
+            foreach (var item in questionsRootObject.results)
+            {
+                listBox1.Items.Add(item.question);
+            }
         }
     }
 }
